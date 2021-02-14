326ae88c4a00ecfaada6752eaaeee949eded9e0b
Ravish Raj Tiwary <ravishrajtiwary21@gmail.com>
2021-02-14 20:53:44 +0530

    initial commit

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define pb push_back
+#define F first
+#define S second
+#define rep(i,j,k) for(ll i=j;i<k;i++)
+#define newl "\n" 
+
+void solve()
+{
+ 
+}
+
+int main()
+{
+  ios_base::sync_with_stdio(false); cin.tie(NULL);
+
+    int t;
+    t = 1;
+    //cin >> t;
+    while (t-- > 0)
+    {
+    solve();
+    }
+
+    return 0;
+}
+ 
